becb829905f10b1f09d1d77cf8406d410be9f93b
Ahmed2398 <ahmedashraf2398@gmail.com>
2023-10-21 22:53:32 +0300

    fix bug

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -3,59 +3,49 @@ import { useFormik } from 'formik'
 import React, { useState } from 'react'
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from "yup";
 const ForgetPassword = () => {
+    let navigate = useNavigate();
+    let [isLoading, setIsLoading] = useState(false);
+    let [errorMessage, setErrorMessage] = useState("")
 
-
-
-
-        const navigate = useNavigate();
-
-        const [loading, setLoading] = useState(false);
-
-        let forgetPass = useFormik({
-            initialValues : {
-                "email":""
-            },
-            onSubmit: function( values ){
-                console.log('Submit',values);
-                forgetPassword( values );
-            }});
-
-
-        async function forgetPassword(obj){
-            setLoading(true);
-            try {
-                const {data} = await axios.post(`https://route-ecommerce.onrender.com/api/v1/auth/forgotPasswords`,obj)
-                console.log(data);
-                setLoading(false);
-                if(data.statusMsg == 'success'){
-                    toast.success(data.message,{duration:2000,className:"text-success px-4 fw-bolder"});
-                    navigate('/verifycode');
-                }
-
-            } catch (error) {
-                console.log('Error : ',error);
-                setLoading(false);
-                toast.error(error.response.data.message,{duration:2000,className:"text-danger px-4 fw-bolder"});
-            }
+    async function forgetPassword(values){
+        setIsLoading(true);
+        let {data} = await  axios.post(`https://route-ecommerce.onrender.com/api/v1/auth/forgotPasswords`, values).catch((err)=>{
+            setErrorMessage(err.response.data.message)
+            setIsLoading(false)
+        })
+        if (data.message === "success") {
+            toast.success(data.message,{duration:2000,className:"text-success px-4 fw-bolder"});
+            navigate('/verifycode');
         }
+            setIsLoading(false)
+    }
+
+    let validationSchema = Yup.object({
+        email: Yup.string().required('Email is required').matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Enter valid email'),
+    })
 
 
+    let formik = useFormik({
+        initialValues:{
+            email: ""
+        },
+        onSubmit: forgetPassword,
+        validationSchema
+    })
 
 
-        return (
+    return (
             <>
-
-
-            <form onSubmit={forgetPass.handleSubmit}>
+            <form onSubmit={forgetPassword.handleSubmit}>
                 <div className="container my-5 py-5">
                     <label className='mt-5 fw-bolder' name='email' htmlFor="email">Enter Your Email</label>
-                    <input onBlur={forgetPass.handleBlur} onChange={forgetPass.handleChange} value={forgetPass.values.email} id="email" type="email" placeholder='Enter Your Email' className='form-control my-2'  />
-
-                    {forgetPass.errors.email && forgetPass.touched.email ?<div className='alert alert-danger text-center '>{ forgetPass.errors.email }</div>:"" }
-                    {loading ? <button type='button' className='btn btn-outline-success mt-3 fw-bolder'>
+                    <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} id="email" type="email" placeholder='Enter Your Email' className='form-control my-2'  />
+                    {formik.errors.email && formik.touched.email ?<div className='alert alert-danger text-center '>{ formik.errors.email }</div>:"" }
+                    {isLoading ? <button type='button' className='btn btn-outline-success mt-3 fw-bolder'>
                         <i className='fa-solid fa-spinner fa-spin text-success mx-2'></i>
-                    </button> : <button disabled={!forgetPass.isValid } type='submit' className='btn btn-outline-success mt-3 fw-bolder'>Send Code</button> }
+                    </button> : <button disabled={!formik.isValid } type='submit' className='btn btn-outline-success mt-3 fw-bolder'>Send Code</button> }
                 </div>
             </form>
 
